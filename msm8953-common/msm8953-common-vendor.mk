100Daisy/android_vendor_xiaomi_daisy

msm8953-common: Update WFD blobs from LA.UM.8.6.r1-01900-89xx.0

msm8953-common-vendor.mk
@@ -55,12 +55,14 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/msm8953-common/proprietary/lib/libqmi_cci_system.so:system/lib/libqmi_cci_system.so \
     vendor/xiaomi/msm8953-common/proprietary/lib/librcc.so:system/lib/librcc.so \
     vendor/xiaomi/msm8953-common/proprietary/lib/libwfdavenhancements.so:system/lib/libwfdavenhancements.so \
+    vendor/xiaomi/msm8953-common/proprietary/lib/libwfdclient.so:system/lib/libwfdclient.so \
     vendor/xiaomi/msm8953-common/proprietary/lib/libwfdcodecv4l2.so:system/lib/libwfdcodecv4l2.so \
     vendor/xiaomi/msm8953-common/proprietary/lib/libwfdcommonutils.so:system/lib/libwfdcommonutils.so \
     vendor/xiaomi/msm8953-common/proprietary/lib/libwfdconfigutils.so:system/lib/libwfdconfigutils.so \
     vendor/xiaomi/msm8953-common/proprietary/lib/libwfdmminterface.so:system/lib/libwfdmminterface.so \
     vendor/xiaomi/msm8953-common/proprietary/lib/libwfdmmsink.so:system/lib/libwfdmmsink.so \
     vendor/xiaomi/msm8953-common/proprietary/lib/libwfdmmsrc.so:system/lib/libwfdmmsrc.so \
+    vendor/xiaomi/msm8953-common/proprietary/lib/libwfdnative.so:system/lib/libwfdnative.so \
     vendor/xiaomi/msm8953-common/proprietary/lib/libwfdrtsp.so:system/lib/libwfdrtsp.so \
     vendor/xiaomi/msm8953-common/proprietary/lib/libwfdservice.so:system/lib/libwfdservice.so \
     vendor/xiaomi/msm8953-common/proprietary/lib/libwfdsm.so:system/lib/libwfdsm.so \

Ryuytyuu-Greseven/android_vendor_yu

msm8953-common/msm8953-common-vendor.mk
@@ -53,12 +53,14 @@ PRODUCT_COPY_FILES += \
     vendor/yu/msm8953-common/proprietary/lib/libqmi_cci_system.so:system/lib/libqmi_cci_system.so \
     vendor/yu/msm8953-common/proprietary/lib/librcc.so:system/lib/librcc.so \
     vendor/yu/msm8953-common/proprietary/lib/libwfdavenhancements.so:system/lib/libwfdavenhancements.so \
+    vendor/yu/msm8953-common/proprietary/lib/libwfdclient.so:system/lib/libwfdclient.so \
     vendor/yu/msm8953-common/proprietary/lib/libwfdcodecv4l2.so:system/lib/libwfdcodecv4l2.so \
     vendor/yu/msm8953-common/proprietary/lib/libwfdcommonutils.so:system/lib/libwfdcommonutils.so \
     vendor/yu/msm8953-common/proprietary/lib/libwfdconfigutils.so:system/lib/libwfdconfigutils.so \
     vendor/yu/msm8953-common/proprietary/lib/libwfdmminterface.so:system/lib/libwfdmminterface.so \
     vendor/yu/msm8953-common/proprietary/lib/libwfdmmsink.so:system/lib/libwfdmmsink.so \
     vendor/yu/msm8953-common/proprietary/lib/libwfdmmsrc.so:system/lib/libwfdmmsrc.so \
+    vendor/yu/msm8953-common/proprietary/lib/libwfdnative.so:system/lib/libwfdnative.so \
     vendor/yu/msm8953-common/proprietary/lib/libwfdrtsp.so:system/lib/libwfdrtsp.so \
     vendor/yu/msm8953-common/proprietary/lib/libwfdservice.so:system/lib/libwfdservice.so \
     vendor/yu/msm8953-common/proprietary/lib/libwfdsm.so:system/lib/libwfdsm.so \